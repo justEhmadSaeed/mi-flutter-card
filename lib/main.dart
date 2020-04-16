5c1f1160a169616c4e744d3a131826eb848b941a
Ehmad <justEhmadSaeed@gmail.com>
2020-04-17 02:43:38 +0500

    📦 NEW: ADD fonts & titles

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,23 @@ class MyApp extends StatelessWidget {
                 backgroundColor: Colors.red,
                 backgroundImage: AssetImage('images/Asset_1.png'),
               ),
+              Text(
+                'Ehmad Saeed',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Flutter Developer'.toUpperCase(),
+                style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.white,
+                ),
+              ),
             ],
           ),
         ),
