cf3f549e4a3e6eb6c0339fefcd3fd0dbe38d3b8b
Ehmad <justEhmadSaeed@gmail.com>
2020-04-17 01:41:11 +0500

    👌 IMPROVE: ADD circleAvatar for image

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 60,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/Asset_1.png'),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
